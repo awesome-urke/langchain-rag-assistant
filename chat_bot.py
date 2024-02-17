3e9a2df96348ce6440cb08185805db605b1fb628
Uros <the_uros@yahoo.com>
2024-02-17 21:48:00 +0100

    First commit

---
+++ chat_bot.py
@@ -0,0 +1,58 @@
+import streamlit as st
+from langchain.vectorstores.chroma import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def find_answer(query_text):
+
+    # Prepare the DB.
+    embedding_function = OpenAIEmbeddings()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+
+    model = ChatOpenAI()
+    response_text = model.predict(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\n\nSources: {sources}"
+
+    return formatted_response
+
+
+def main():
+
+    st.write("## ChatBot Assistant POC")
+    query_text = st.text_input("Question", "What is SageMaker?")
+
+    answer = find_answer(query_text)
+
+    if st.button("Click Here"):
+        st.write(answer)
+
+if __name__ == "__main__":
+    main()
